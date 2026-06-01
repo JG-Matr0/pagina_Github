7e3862de71ef195c3b8aa98bd4e5c45a54b9a35e
JotaGe <jesus2832003@gmail.com>
2026-06-01 23:37:08 +0200

    57uu547rui

--- cosa.js
+++ cosa.js
@@ -1,1 +1,22 @@
+// Esperamos a que el DOM esté cargado [4]
+document.addEventListener('DOMContentLoaded', () => {
+
+    let goles = 0;
+    const boton = document.getElementById('botonGol');
+    const marcador = document.getElementById('marcador');
+
+    // Evento de clic para el botón de fútbol
+    if (boton) {
+        boton.addEventListener('click', () => {
+            goles++;
+            // Modificamos el contenido del HTML en tiempo real
+            marcador.innerText = `Goles marcados: ${goles}`;
+
+            // Efecto visual sencillo
+            alert("¡¡¡GOOOOOL!!!");
+            console.log("Se ha registrado un nuevo gol. Total: " + goles);
+        });
+    }
+});
+
 
